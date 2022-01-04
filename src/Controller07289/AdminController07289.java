11b25053d23e9b0eff1ac7fd7331bee4a7d0f2be
hirdhan <hirdhanantama46@gmail.com>
2022-01-04 19:04:38 +0700

    Update AdminController07289.java

--- src/Controller07289/AdminController07289.java
+++ src/Controller07289/AdminController07289.java
@@ -3,8 +3,8 @@ package Controller07289;
 import Entity07289.AdminEntity0728907289;
 
 public class AdminController07289 {
-    public void insertData(int id,String nip,String nama, String alamat, String password, String noTelp){
-        AllObjectModel07289.adminModel07289.insertData(new AdminEntity0728907289(id,nip,nama,alamat,password,noTelp));
+    public void insertData(String nip,String nama, String alamat, String password, String noTelp){
+        AllObjectModel07289.adminModel07289.insertData(new AdminEntity0728907289(nip,nama,alamat,password,noTelp));
     }
     public void updatePassword(String password, int id){
         AllObjectModel07289.adminModel07289.updatePassword(password,id);
@@ -19,4 +19,6 @@ public class AdminController07289 {
     public void deleteData(int id){
         AllObjectModel07289.adminModel07289.deleteData(id);
     }
+
+
 }
